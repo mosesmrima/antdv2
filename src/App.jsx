2d513222333c0f76a3ec09f7563f58049d72b0f1
mrima <mrimamss@gmail.com>
2023-02-02 09:44:18 +0300

    import EmployeesForm

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import EmployeesForm from "./components/EmployeesForm.jsx";
+import "./styles/App.css"
+
+export default function App() {
+    return (
+        <div className={"app"}>
+            <EmployeesForm/>
+        </div>
+    )
+}
