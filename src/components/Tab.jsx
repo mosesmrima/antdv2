8f0653d7918c03e1ddb4e3187e1b810b815a2755
mrima <mrimamss@gmail.com>
2023-02-03 15:39:55 +0300

    add transition layout

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,11 +1,15 @@
 import {Tabs} from "antd"
+import Mock from "./Mock"
 import EmployeesForm from "./EmployeesForm.jsx";
 import "../styles/Tab.css"
 export default function TabView() {
     return (
         <div className={"tab-wrapper-div"}>
             <Tabs defaultActiveKey="1">
-                <Tabs.TabPane tab="Employees" key="1">
+                <Tabs.TabPane tab="Layout 1" key="1">
+                  <Mock/>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Layout 2" key="2">
                     <EmployeesForm/>
                 </Tabs.TabPane>
             </Tabs>
